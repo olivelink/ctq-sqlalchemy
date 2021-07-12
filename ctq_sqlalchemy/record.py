5b17472b9245dec06ceba7d88445dfeda4ad3661
Adam Terrey <arterrey@gmail.com>
2021-07-12 17:39:24 +0700

    Fix record extras

--- ctq_sqlalchemy/record.py
+++ ctq_sqlalchemy/record.py
@@ -6,14 +6,14 @@ class RecordExtras:
     def edit(self, **kwargs):
         """Proxy the edit function on the parent collection object.
         """
-        return acquire(self).edit(self, *args, **kwargs)
+        return acquire(self.__parent__).edit(self, *args, **kwargs)
 
     def rename(self, name):
         """Proxy the rename function on the parent collection object.
         """
-        return acquire(self).rename(self,name)
+        return acquire(self.__parent__).rename(self,name)
     
     def delete(self):
         """Proxy the delete function on the parent collection object.
         """
-        return acquire(self).delete(self)
+        return acquire(self.__parent__).delete(self)
