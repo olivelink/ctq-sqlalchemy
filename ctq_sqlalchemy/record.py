359e970d66943838c434a66ff11fb7b48f8a9a06
Adam Terrey <arterrey@gmail.com>
2021-07-14 06:34:56 +0700

    Fix record edit method

--- ctq_sqlalchemy/record.py
+++ ctq_sqlalchemy/record.py
@@ -6,12 +6,12 @@ class RecordExtras:
     def edit(self, **kwargs):
         """Proxy the edit function on the parent collection object.
         """
-        return acquire(self.__parent__).edit(self, *args, **kwargs)
+        return acquire(self.__parent__).edit(self, **kwargs)
 
     def rename(self, name):
         """Proxy the rename function on the parent collection object.
         """
-        return acquire(self.__parent__).rename(self,name)
+        return acquire(self.__parent__).rename(self, name)
     
     def delete(self):
         """Proxy the delete function on the parent collection object.
